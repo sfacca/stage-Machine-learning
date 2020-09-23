026ba5863f361bd1416dade740418a73aed2533d
sebastiano facca <46827183+sfacca@users.noreply.github.com>
2020-09-23 18:48:07 +0200

    fine giornata
    andato avanti a convertire pr_convert

--- codice/julia/lists.jl
+++ codice/julia/lists.jl
@@ -3,36 +3,31 @@
 ### dove next è puntatore all' elemento prossimo nell ordine e 
 ### value è il dato che effettivamente vogliamo salvare
 
+module lists
 
-
+export lnode, List, lpush!, lappend!, lget, printList
 
 mutable struct lnode
     value::Any
     next::Union{lnode,Nothing}
-    lnode(v) = (x = new(); x.next = x; x.value = v; x)
-    lnode(v::Any,next::lnode) = (x = new(); x.next = next; x.value = v; x)
 end
 # per lista dopiam collegata basta aggiungere un campo parent::lnode 
 
 mutable struct List
     first::lnode
     last::lnode
-    List(nodo::lnode) = List(nodo,nodo)
-    List(first::lnode, last::lnode) = (x = new(); x.first = first; x.last=last; x)
 end
 # a seconda dell uso può esser utile aggiungere campi come length e puntatore a ultimo elemento
-# il vantaggio è che è una struttura ordinata nella quale ogni allocazione di nuovi dati impiega tempo costante 
-# (mentre, ad esempio, se devi aggiungere un elemento a un array pieno devi reallocarti tutto l'array allocandogli più spazio)
-
+#
 
 function lpush!(list::List, value::Any)
     newnode = lnode(value, list.first) # crea nodo nuovo
-    list.last.next = newnode # convenzione: ultimo elemento punta a primo elemento
+    #list.last.next = newnode
     list.first = newnode # lo aggiounge in testa alla lista
 end
 
 function lappend!(list::List, value::Any)
-    newnode = lnode(value, list.first)# convenzione: ultimo elemento punta a primo elemento)
+    newnode = lnode(value, nothing)
     list.last.next= newnode# aggiorna puntatore next dell ultimo elemento, effettivamente aggiungendo nuovo elemento alla lista
     list.last = newnode #segnala a lista che il nostro nuovo nono è ultimo elemento
 end
@@ -70,43 +65,16 @@ function lget(list::List, value::Any)
             error("valore non trovato")
         end=#
     temp
-end  
-
-function dumpvalues(list::List)
-    tempnode = list.first
-    while (tempnode.next != list.first)
-        println(tempnode.value)
-        tempnode = tempnode.next
-    end
-    println(tempnode.value)
-end
-        
+end    
 # NB: se la lista è lista di int lget(lista,numero) usa lget sull indice invece il lget su valore
 
-
-# per convenzione ultimo elemento lista punta a primo elemento lista (lista circolare)
-
-
-function tryme()
-    mynode::lnode=lnode("secondo!")
-    myl::List = List(mynode)
-    @show myl
-    # aggiungiamo un nodo con valore terzo! alla fine della lista
-    lappend!(myl, "terzo!")
-    @show myl
-    #aggiungiamo nodo primo! all inizio della lista
-    lpush!(myl, "primo!")
-    @show myl
-    mynode = myl.first
-    dumpvalues(myl)
-        
+function printList(list::List)
+    mynode = list.first
+    while mynode.next!=nothing
+        println(mynode.value)
+        mynode = mynode.next
+    end
+    println(mynode.value)#print ultimo elemento
 end
-tryme()
 
-# i puntatori in julia non funzionano così
-# julia non ha puntatori!
-# dovevo definirla come oggetto lista!
-
-
-
-
+end
