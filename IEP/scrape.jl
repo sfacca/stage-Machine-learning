cc4fea4397f8e6705cd94961036fe33eefc5584f
Sebastiano Facca <830479@stud.unive.it>
2021-01-24 21:58:02 +0100

    fine giornata

--- IEP/scrape.jl
+++ IEP/scrape.jl
@@ -13,24 +13,11 @@ using Match
 # ╔═╡ 65b70120-5caa-11eb-05a9-bd4239022c3e
 include("corpus_struct.jl")
 
-# ╔═╡ 6a656b30-5cf0-11eb-37f3-a3c326b4c4b0
-function scrape_expr(e::CSTParser.EXPR; d = nothing)
-	if e.head in [:function, :struct, :abstract] || !isnothing(d)
-		res = [(docs = d, type = e.head, content = handle_Content(e))]
-	else
-		res = []
-	end
-	docs = nothing
-	# CSTParser.EXPR is iterable, needs no checks on args
-	for subexpr in e
-		if doc
-			docs = e
-		else
-		res = vcat(res, scrape_expr(e, d))
-		docs = nothing
-	end
-	res
-end
+# ╔═╡ e43e5ce0-5e7c-11eb-2a12-bf68ab971179
+include("parse_folder.jl")
+
+# ╔═╡ 4f974a10-5e7d-11eb-277a-f7db47dbc06b
+CSTParser.EXPR
 
 # ╔═╡ 069ea9e0-5cf0-11eb-3d1b-2149bf325319
 """
@@ -39,6 +26,37 @@ fuynction to scrape from CSTParser.EXPR
 function scrape_Docs(e::CSTParser.EXPR)
 	# CSTParser.EXPR is iterable, no need for .args size checks	
 	res = ()
+end
+
+# ╔═╡ 70b8e320-5e7d-11eb-3279-8115a509cf96
+function get_inputs(e::CSTParser.EXPR)
+	# alg is similar to Expr
+	#1 find :call expression
+	call_ind = findfirst((x)->(x.head == :call), e)
+	println("call_ind $call_ind")
+	if isnothing(call_ind)
+		#there is no :call
+		return ("could not find :call subexpr")
+	else
+		
+		call = e.args[call_ind]
+		println("call: $call")
+		# find variables
+		# first element should be name of function
+		name = call.args[1]
+		#the rest are variables, defined either with :IDENTIFIER or :OP (for variables of set types)		
+		variables = length(call)>1 ? call[2:end] : []
+		
+		#=
+		return (
+			name = name, 
+			input_variables = [
+				x.head == :IDENTIFIER ? x.val : (name = x[1].val, type = x[2].val)
+				for x in variables
+			]
+		)=#
+		return ()
+	end
 end
 
 # ╔═╡ 62f8bd22-5a89-11eb-3158-cba0a98fb785
@@ -97,7 +115,7 @@ end
 """
 middleware to handle different kind of Expr differently
 """
-function handle_Content(e::Expr)
+function handle_Content(e::Union{Expr, CSTParser.EXPR})
 	@match e.head begin
 		:function => get_inputs(e)
 		_ => e
@@ -130,6 +148,36 @@ function scrape_expr(e::Expr; d = nothing)
 				docs = nothing# attaches docs to next expression, then clears docs
 			end
 		end	
+	end
+	res
+end
+
+# ╔═╡ 6a656b30-5cf0-11eb-37f3-a3c326b4c4b0
+function scrape_expr(e::CSTParser.EXPR, d = nothing)
+	if e.head in [:function, :struct, :abstract] || !isnothing(d)
+		res = [(docs = d, type = e.head, content = handle_Content(e))]
+		d = nothing
+		#clear docs after assigning them
+	else
+		res = []
+	end
+	
+	# CSTParser.EXPR is iterable, needs no checks on args
+	i = 1
+	while i <= length(e)
+		if e[i].head == :globalref
+			next_string = findfirst((x)->(x.head == :TRIPLESTRING), e[i:end])
+			if isnothing(next_string)
+				println("empty documentation definition")
+			else
+				d = e[next_string].val
+				i = next_string
+			end
+		else
+			res = vcat(res, scrape_expr(e, d))
+			d = nothing			
+		end
+		i = i + 1
 	end
 	res
 end
@@ -241,15 +289,98 @@ function make_struct(funcs::Array{Expr,1})
 	res
 end
 
+# ╔═╡ eb625da0-5e7c-11eb-177e-eb48a6f94b6a
+parsed_sample = read_code("programs");
+
+# ╔═╡ 16558b40-5e7d-11eb-2c08-196665490091
+exprs_only = [x[1] for x in parsed_sample];
+
+# ╔═╡ b00b3d3e-5e85-11eb-289b-a912cd960e2b
+exprs_only[1].args[3].val
+
+# ╔═╡ 0d232dd0-5e86-11eb-3bf3-efafd02e7b87
+scrape_expr.(exprs_only)
+
+# ╔═╡ 52157730-5e82-11eb-22c7-496acd08f489
+
+
+# ╔═╡ 884be410-5e7d-11eb-0fb3-33fe00da9b49
+function find_heads(e::CSTParser.EXPR, head::Symbol)
+	if e.head == head
+		res = [e]
+	else
+		res = []
+	end
+	
+	for x in e
+		res = vcat(res, find_heads(x,head))
+	end
+	
+	res
+end	
+
+# ╔═╡ 52b64082-5e81-11eb-3192-5b0f4f436d55
+function find_heads(arr::Array{CSTParser.EXPR,1}, head::Symbol)
+	res = []
+	for x in arr
+		res = vcat(res, find_heads(x, head))
+	end
+	res
+end
+
+# ╔═╡ 25bf0580-5e81-11eb-052f-4f1e5c35349e
+functions = find_heads(exprs_only, :function);
+
+# ╔═╡ 085655be-5e81-11eb-3ac5-b1fd97b0f80b
+scrape_expr(functions[1])
+
+# ╔═╡ 7c9f7b60-5e85-11eb-0c08-8ff862d1fd21
+typeof(functions[1])
+
+# ╔═╡ b1646170-5e81-11eb-07f6-8128df68413a
+length(functions)
+
+# ╔═╡ ac9dffc0-5e81-11eb-06bc-d529446f090e
+functions[1][2].args[1].head
+
+# ╔═╡ c2a49450-5e81-11eb-0c84-0d28904239df
+functions[1][2].args[2].head
+
+# ╔═╡ d7ff1c50-5e7f-11eb-01c0-452cc44a4b0a
+"""
+overloads Base.keys so it works with CSTParser.EXPR
+this is required to make findfirst work with CSTParser.EXPR
+"""
+function Base.keys(e::CSTParser.EXPR)
+	return keys(e.args)
+end
+
 # ╔═╡ Cell order:
 # ╠═65b70120-5caa-11eb-05a9-bd4239022c3e
+# ╠═4f974a10-5e7d-11eb-277a-f7db47dbc06b
 # ╠═db681500-5a5a-11eb-0de7-fd488e8ecb46
 # ╠═6a656b30-5cf0-11eb-37f3-a3c326b4c4b0
+# ╠═b00b3d3e-5e85-11eb-289b-a912cd960e2b
+# ╠═0d232dd0-5e86-11eb-3bf3-efafd02e7b87
 # ╠═5af37660-5a84-11eb-39b1-7bcd6c24d815
 # ╠═1341adf0-5cf0-11eb-2779-21605d9879c9
 # ╠═069ea9e0-5cf0-11eb-3d1b-2149bf325319
 # ╠═5a8dbd70-5a89-11eb-104f-192cb5f012d7
 # ╠═c1acf890-5a89-11eb-2b8c-cbde52b367b7
 # ╠═5e799da0-5a89-11eb-28a3-6fc5463b63e4
+# ╠═70b8e320-5e7d-11eb-3279-8115a509cf96
+# ╠═085655be-5e81-11eb-3ac5-b1fd97b0f80b
+# ╠═7c9f7b60-5e85-11eb-0c08-8ff862d1fd21
 # ╠═62f8bd22-5a89-11eb-3158-cba0a98fb785
 # ╠═70a65360-5a89-11eb-2898-37b1b52852bf
+# ╠═e43e5ce0-5e7c-11eb-2a12-bf68ab971179
+# ╠═eb625da0-5e7c-11eb-177e-eb48a6f94b6a
+# ╠═16558b40-5e7d-11eb-2c08-196665490091
+# ╠═25bf0580-5e81-11eb-052f-4f1e5c35349e
+# ╠═b1646170-5e81-11eb-07f6-8128df68413a
+# ╠═ac9dffc0-5e81-11eb-06bc-d529446f090e
+# ╠═c2a49450-5e81-11eb-0c84-0d28904239df
+# ╠═52157730-5e82-11eb-22c7-496acd08f489
+# ╠═884be410-5e7d-11eb-0fb3-33fe00da9b49
+# ╠═52b64082-5e81-11eb-3192-5b0f4f436d55
+# ╠═d7ff1c50-5e7f-11eb-01c0-452cc44a4b0a
