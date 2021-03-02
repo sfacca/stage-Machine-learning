9cdfb5b8a541033c43b009d7b61f06d832a50034
Sebastiano Facca <830479@stud.unive.it>
2021-03-02 16:11:52 +0100

    inizio TextAnalysis docstring

---
+++ IEP/src/corpus/docstring/doc_fun.jl
@@ -0,0 +1,6 @@
+
+struct doc_fun
+    doc::String
+    fun::String
+    doc_fun(a,b) = new(a, b);
+end
---
+++ IEP/src/corpus/docstring/make_corpus.jl
@@ -0,0 +1,18 @@
+using Pkg
+Pkg.activate("../.")
+include("doc_fun.jl")
+
+using TextAnalysis
+
+doc_funs = load("docstring/doc_funs.jld2")["doc_funs"]
+
+
+strings = [StringDocument(x.doc) for x in doc_funs]
+names = [StringDocument(x.fun) for x in doc_funs]
+
+crps = Corpus(strings)
+update_lexicon!(crps)
+m = DocumentTermMatrix(crps)
+dense = dtm(m, :dense)
+
+#standardize!(crps, NGramDocument)
