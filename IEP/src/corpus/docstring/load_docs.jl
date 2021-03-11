35eec95af314d74e2b4ef29bc7d5843d5d7ae561
Sebastiano Facca <830479@stud.unive.it>
2021-03-11 01:50:04 +0100

    PerceptronTagger does not exist in the pkg

--- IEP/src/corpus/docstring/load_docs.jl
+++ IEP/src/corpus/docstring/load_docs.jl
@@ -2,11 +2,7 @@ using JLD2, FileIO
 using TextAnalysis
 include("doc_fun.jl")
 
-if !isnothing(doc_funs)
-    x_tmp = doc_funs
-else
-    x_tmp = nothing
-end
+
 
 doc_funs = FileIO.load("doc_funs.jld2")["doc_funs"]
 doc_funs = filter((x)->(x.doc != "error finding triplestring"),doc_funs)
@@ -15,6 +11,6 @@ doc_funs = filter((x)->(x.doc != "error finding triplestring"),doc_funs)
 
 strings = [StringDocument(x.doc) for x in doc_funs]
 names = [StringDocument(x.fun) for x in doc_funs]
-doc_funs = x_tmp
+doc_funs = nothing
 
 "strings contains the docstrings, names contains the function names"
---
+++ IEP/src/corpus/docstring/make_corpus.jl
@@ -0,0 +1,28 @@
+using Pkg
+Pkg.activate(".")
+#include("../corpus.jl")
+include("doc_fun.jl")
+include("tokenize.jl")
+include("bag_of_words.jl")
+
+using JLD2, FileIO
+using TextAnalysis
+
+include("load_docs.jl")
+
+crps = Corpus(strings)
+update_lexicon!(crps)
+
+#=
+crps_1 = Corpus(strings[1:asd])
+crps_2 = Corpus(strings[asd:2*asd])
+crps_3 = Corpus(strings[2*asd:3*asd])
+crps_4 = Corpus(strings[3*asd:end])
+=#
+
+#update_lexicon!(crps)
+
+#"crps, crps_1, crps_2, crps_3, crps_4"
+#lsa(crps_2)
+#==#
+#standardize!(crps, NGramDocument)
